7b05dd6402d7fad54a5fab77cc501207c5310d42
Muhammed paqer ahmed abbas <moh999157@gmail.com>
2021-03-02 08:22:41 +0300

    Header added

---
+++ assets/js/init.js
@@ -0,0 +1,7 @@
+new fullpage('#fullpage', {
+  // navigation:true,
+  // navigationPosition:'right',
+  // controlArrows:false,
+    // fadingEffect: true,
+    // sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+  });
